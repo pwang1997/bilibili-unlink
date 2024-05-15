589a2277bd643ec18a958e3a47881e07ac0ab199
Zhengliang Wang <puck19970418@gmail.com>
2024-05-14 22:28:17 -0400

    add: get orphan bvs

--- app.go
+++ app.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 // App struct
@@ -21,11 +24,76 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-// func (a *App) Greet(name string) string {
-// 	return fmt.Sprintf("Hello %s, It's show time!", name)
-// }
+type Media struct {
+	Bvid  string `json:"bvid"`
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+}
 
-func (a *App) DissociateOrphanFav(jct string, fid string, mediumId string) string {
-	return fmt.Sprintf("Go Server received jct: %s, fid: %s, mediumId %s", jct, fid, mediumId)
+type Data struct {
+	Has_more bool    `json:"has_more"`
+	TTL      int     `json:"ttl"`
+	Medias   []Media `json:"medias"`
+}
+
+type FavResponse struct {
+	Code int  `json:"code"`
+	TTL  int  `json:"ttl"`
+	Data Data `json:"data"`
+}
+
+func getFav(mediumId string, pageNumber int, pageSize int) FavResponse {
+	requestUrl := fmt.Sprintf("https://api.bilibili.com/x/v3/fav/resource/list?media_id=%s&pn=%d&ps=%d&keyword=&order=mtime&type=0&tid=0", mediumId, pageNumber, pageSize)
+	resp, err := http.Get(requestUrl)
+	if err != nil {
+		fmt.Println("Error making GET request:", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+
+	var favResponse FavResponse
+	if err := json.Unmarshal(body, &favResponse); err != nil {
+		fmt.Println("Error parsing JSON:", err)
+	}
+
+	return favResponse
+}
+
+func getUnavailableBVs(mediumId string, pageSize int) []string {
+
+	var pageNumber = 1
+	var hasMore = true
+	var bvIdToDelete []string
+
+	for hasMore {
+		var favResponse FavResponse = getFav(mediumId, pageNumber, pageSize)
+
+		var data = favResponse.Data
+		var mediaList = data.Medias
+		hasMore = data.Has_more
+
+		fmt.Println("PageNumber: ", pageNumber, ", has more to fetch: ", hasMore)
+
+		for i := 0; i < len(mediaList); i++ {
+			var Title = mediaList[i].Title
+
+			if Title == "已失效视频" {
+				bvIdToDelete = append(bvIdToDelete, mediaList[i].Bvid)
+			}
+		}
+		pageNumber++
+	}
+
+	return bvIdToDelete
+}
+
+func (a *App) DissociateOrphanFav(mediumId string) {
+	pageSize := 20
+
+	var bvIdToDelete []string = getUnavailableBVs(mediumId, pageSize)
+
+	fmt.Println("Bvs to delete: ", bvIdToDelete)
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,24 +5,14 @@ import './App.css';
 export default function App() {
     const [requestState, setRequestState] = useState<string>('');
 
-    const jctRef = useRef();
-    const fidRef = useRef();
     const mediumIdRef = useRef();
-
-    const handleUpdateJct = useCallback((e: any) => {
-        jctRef.current = e.target.value;
-    }, [])
-
-    const handleUpdateFid = useCallback((e: any) => {
-        fidRef.current = e.target.value;
-    }, []);
 
     const handleUpdateMediumIdRef = useCallback((e: any) => {
         mediumIdRef.current = e.target.value;
     }, []);
 
     const handleDissociateOrphanFav = async () => {
-        await DissociateOrphanFav(jctRef.current ?? '', fidRef.current ?? '', mediumIdRef.current ?? '').then((res: any) => {
+        await DissociateOrphanFav(mediumIdRef.current ?? '').then((res: any) => {
             console.log(res);
         })
     }
@@ -41,17 +31,6 @@ export default function App() {
     return (
         <div id="App">
             <div style={{ display: "flex", flexDirection: "column", gap: "16px", maxWidth: "400px", margin: "auto" }}>
-                <div id="bili-jct-input" className="input-box" style={{ display: "flex", justifyContent: "space-between" }}>
-                    <label htmlFor='bili-jct'>Cookie Token[bili_jct]</label>
-                    <input id="bili-jct" className="input" onChange={handleUpdateJct} autoComplete="off" name="input" type="text" />
-                </div>
-
-                <div id="bili-fid-input" className="input-box" style={{ display: "flex", justifyContent: "space-between" }}>
-                    <label htmlFor='bili-fid'>Fid</label>
-
-                    <input id="bili-fid" className="input" onChange={handleUpdateFid} autoComplete="off" name="input" type="text" />
-                </div>
-
                 <div id="bili-medium-id-input" className="input-box" style={{ display: "flex", justifyContent: "space-between" }}>
                     <label htmlFor='bili-medium-id'>Medium Id</label>
 
@@ -59,7 +38,7 @@ export default function App() {
                 </div>
             </div>
 
-            <button className="btn" onClick={handleOnClick} disabled={requestState === 'IN_PROGRESS'}>Submit</button>
+            <button className="btn" onClick={handleOnClick}>Submit</button>
 
         </div>
     )
